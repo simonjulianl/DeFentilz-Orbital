b759730499a17254ad7472db6e1b0b0813830fa6
lauwsj <lauwsj@sea.com>
2021-05-24 22:48:35 +0800

    Fix bug

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -14,10 +14,10 @@ import requestpwdModal from "../ReqPwdModal/ReqPwdModal";
 const Header: React.FC<{}> = () => {
   const router = useRouter();
   const authContext = useAuth();
-  const [name, setName ] = useState("");
+  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [error, setError] = useState({errorCode: null, errorMessage: null});
+  const [error, setError] = useState({ errorCode: null, errorMessage: null });
   const {
     isOpen: isOpenLogin,
     onOpen: onOpenLogin,
@@ -33,15 +33,37 @@ const Header: React.FC<{}> = () => {
     onOpen: onOpenPwd,
     onClose: onClosePwdTemp,
   } = useDisclosure();
-  const resetError = () => setError({errorCode: null, errorMessage: null});
-  const onCloseLogin = () => { resetError(); onCloseLoginTemp(); }
-  const onCloseSignup = () => { resetError(); onCloseSignupTemp(); }
-  const onClosePwd = () => { resetError(); onClosePwdTemp(); }
+  const resetError = () => setError({ errorCode: null, errorMessage: null });
+  const onCloseLogin = () => {
+    resetError();
+    onCloseLoginTemp();
+  };
+  const onCloseSignup = () => {
+    resetError();
+    onCloseSignupTemp();
+  };
+  const onClosePwd = () => {
+    resetError();
+    onClosePwdTemp();
+  };
 
-  const hookVars = {name, email, password, error};
-  const settersObject = {setName, setEmail, setPassword, setError};
-  const modalCallbacks = { onOpenLogin, onCloseLogin, onOpenSignup, onCloseSignup, onOpenPwd, onClosePwd};
-  const handlerObject = authHandlers(hookVars, settersObject, modalCallbacks, authContext, router); 
+  const hookVars = { name, email, password, error };
+  const settersObject = { setName, setEmail, setPassword, setError };
+  const modalCallbacks = {
+    onOpenLogin,
+    onCloseLogin,
+    onOpenSignup,
+    onCloseSignup,
+    onOpenPwd,
+    onClosePwd,
+  };
+  const handlerObject = authHandlers(
+    hookVars,
+    settersObject,
+    modalCallbacks,
+    authContext,
+    router
+  );
 
   return (
     <>
@@ -55,60 +77,60 @@ const Header: React.FC<{}> = () => {
           <Image src="/4.png" fit="contain" alt="BoNUS Logo" boxSize="100px" />
         </Box>
         <Box justifyContent="space-around" align="center">
-        {authContext.auth ? (
-          <HStack paddingRight="5">
-            {GeneralButton(
-            {label : "Log Out", path : null, icon : null},
-            handlerObject.logOutHandler,
-            router
+          {authContext.auth ? (
+            <HStack paddingRight="5">
+              {GeneralButton(
+                { label: "Log Out", path: null, icon: null },
+                handlerObject.logOutHandler,
+                router
+              )}
+            </HStack>
+          ) : (
+            <HStack paddingRight="5" justifyContent="space-between">
+              {GeneralButton(
+                {
+                  icon: null,
+                  path: "/signup",
+                  label: "Sign Up",
+                },
+                onOpenSignup,
+                router
+              )}
+              {GeneralButton(
+                {
+                  icon: null,
+                  path: "/signin",
+                  label: "Log In",
+                },
+                onOpenLogin,
+                router
+              )}
+              <Modal isOpen={isOpenLogin} onClose={onCloseLogin}>
+                {signinModal(
+                  isOpenLogin,
+                  onCloseLogin,
+                  handlerObject,
+                  hookVars
+                )}
+              </Modal>
+              <Modal isOpen={isOpenSignup} onClose={onCloseSignup}>
+                {signupModal(
+                  isOpenSignup,
+                  onCloseSignup,
+                  handlerObject,
+                  hookVars
+                )}
+              </Modal>
+              <Modal isOpen={isOpenPwd} onClose={onClosePwd}>
+                {requestpwdModal(
+                  isOpenPwd,
+                  onClosePwd,
+                  handlerObject,
+                  hookVars
+                )}
+              </Modal>
+            </HStack>
           )}
-          </HStack>
-        ) : (
-          <HStack paddingRight="5" justifyContent="space-between">
-            {GeneralButton(
-              {
-                icon : null,
-                path: "/signup",
-                label: "Sign Up",
-              },
-              onOpenSignup,
-              router
-            )}
-            {GeneralButton(
-              {
-                icon : null,
-                path: "/signin",
-                label: "Log In",
-              },
-              onOpenLogin,
-              router
-            )}
-            <Modal isOpen={isOpenLogin} onClose={onCloseLogin}>
-              {signinModal(
-                isOpenLogin,
-                onCloseLogin,
-                handlerObject,
-                hookVars
-              )}
-            </Modal>
-            <Modal isOpen={isOpenSignup} onClose={onCloseSignup}>
-              {signupModal(
-                isOpenSignup,
-                onCloseSignup,
-                handlerObject,
-                hookVars
-              )}
-            </Modal>
-            <Modal isOpen={isOpenPwd} onClose={onClosePwd}>
-              {requestpwdModal(
-                isOpenPwd,
-                onClosePwd,
-                handlerObject,
-                hookVars
-              )}
-            </Modal>
-          </HStack>
-        )}
         </Box>
       </Flex>
     </>
