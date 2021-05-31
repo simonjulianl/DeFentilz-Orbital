b5226af699ec8e3656be664d3b4bd12e548a7fdb
Amadeus <amadeusw6@gmail.com>
2021-06-01 00:32:44 +0800

    Fix Type Error

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,15 +1,13 @@
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
 import React from 'react';
-import { chakra } from "@chakra-ui/react"
+import { chakra, forwardRef} from "@chakra-ui/react"
 
-const ChakraCarousel = chakra(Carousel, {
-    shouldForwardProp: (prop) => !["sample"].includes(prop),
-  });
+const ChakraCarousel = chakra(Carousel);
 
 function MainCarousel (props){
     return (
-        <ChakraCarousel autoPlay axis={"horizontal"} width={props.width} infiniteLoop={true} showThumbs={false} showArrows={false}>
+        <ChakraCarousel autoPlay axis={"horizontal"} width={props.width} infiniteLoop={true} showThumbs={false} showArrows={false} {...props}>
             <div>
                 <img width="200px" height="100px" src="http://lorempixel.com/output/cats-q-c-640-480-1.jpg" />
             </div>
@@ -25,5 +23,6 @@ function MainCarousel (props){
         </ChakraCarousel>
     );
 }
+
 
 export default MainCarousel;
