6fccd281d9548ce35d2be2aa7aa1134d3c8c0273
Simon Julian Lauw <lauwsj@sea.com>
2021-06-05 22:36:01 +0800

    Fix icon bug

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -11,6 +11,13 @@ import SignUpModal from "../SignUpModal/SignUpModal";
 import SignInModal from "../SignInModal/SignInModal";
 import ReqPwdModal from "../ReqPwdModal/ReqPwdModal";
 
+// The following import prevents a Font Awesome icon server-side rendering bug,
+// where the icons flash from a very large icon down to a properly sized one:
+import "@fortawesome/fontawesome-svg-core/styles.css";
+// Prevent fontawesome from adding its CSS since we did it manually above:
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false; /* eslint-disable import/first */
+
 const Header: React.FC<{}> = () => {
   const router = useRouter();
   const authContext = useAuth();
