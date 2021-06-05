844c42493563910022e26fd338a4cb276dfed8cc
Simon Julian Lauw <lauwsj@sea.com>
2021-06-06 01:14:24 +0800

    Bug : weak password never handled

--- src/components/BonusAlert/AlertConfig.tsx
+++ src/components/BonusAlert/AlertConfig.tsx
@@ -1,2 +1,3 @@
 export const USER_NOT_FOUND = "auth/user-not-found";
 export const PASSWORD_CHANGE_SUCCESS = "password_change_success";
+export const WEAK_PASSWIRD = "auth/weak-password ";
--- src/components/BonusAlert/BonusAlert.tsx
+++ src/components/BonusAlert/BonusAlert.tsx
@@ -1,5 +1,6 @@
 import { Alert, AlertIcon, AlertTitle } from "@chakra-ui/react";
-import { PASSWORD_CHANGE_SUCCESS, USER_NOT_FOUND } from "./AlertConfig";
+import { WEAK_PASSWORD } from "./AlertConfig";
+import { PASSWORD_CHANGE_SUCCESS, USER_NOT_FOUND, 7WEAK_PASSWIRD } from "./AlertConfig";
 
 interface OwnProps {
   status: "error" | "info" | "warning" | "success";
@@ -13,6 +14,8 @@ const BonusAlert: React.FC<OwnProps> = ({ status, code }) => {
         return "User Not Found";
       case PASSWORD_CHANGE_SUCCESS:
         return "We have sent a password change link to the above email!";
+      case WEAK_PASSWORD:
+        return "Password should be at least 6 characters long"
       default:
         return "Unhandled Error";
     }
