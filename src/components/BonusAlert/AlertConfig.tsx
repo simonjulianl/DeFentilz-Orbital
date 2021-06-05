44be6f6437d19c022d0a3e25e4fdbfdb9b9fa9bd
Simon Julian Lauw <lauwsj@sea.com>
2021-06-06 01:16:21 +0800

    Bug : weak password

--- src/components/BonusAlert/AlertConfig.tsx
+++ src/components/BonusAlert/AlertConfig.tsx
@@ -1,3 +1,3 @@
 export const USER_NOT_FOUND = "auth/user-not-found";
 export const PASSWORD_CHANGE_SUCCESS = "password_change_success";
-export const WEAK_PASSWIRD = "auth/weak-password ";
+export const WEAK_PASSWORD = "auth/weak-password ";
