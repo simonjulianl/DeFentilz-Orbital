c2c2df55873bc4ddb3ab1fb9d7e2dd8f9b5ecc9a
Simon Julian Lauw <lauwsj@sea.com>
2021-06-11 14:02:45 +0800

    Hotfix : opening cors to everyone

--- backend/index.js
+++ backend/index.js
@@ -12,10 +12,12 @@ const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 5000; // heroku port
 
-var corsOptions = {
+const corsOptions = {
   // for extension
-  origin: "https://bonus.vercel.app",
-  optionsSuccessStatus: 200,
+  origin: "*",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
 };
 
 app.use(cors(corsOptions));
